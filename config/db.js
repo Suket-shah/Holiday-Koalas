757b2495d7bca5ba50dcd0b8674b29d849871220
Suket-shah <54611415+Suket-shah@users.noreply.github.com>
2021-01-08 15:06:30 -0600

    Update DB server config

--- config/db.js
+++ config/db.js
@@ -1,21 +1,41 @@
 const { Connection, Request} = require('tedious');
 
 // DB Connections
-const config = {
+// const config = {
+//     authentication: {
+//         options: {
+//           userName: process.env.SERVER_ADMIN_USER,
+//           password: process.env.SERVER_PASSWORD
+//         },
+//         type: "default"
+//       },
+//       server: process.env.SERVER_NAME, 
+//       options: {
+//         database: process.env.DATABASE_NAME, 
+//         encrypt: true,
+//         rowCollectionOnDone: true
+//     }
+// };
+
+var config = {
+    server: process.env.SERVER_NAME,
+    options: { encrypt: true, database: process.env.DATABASE_NAME },
     authentication: {
-        options: {
-          userName: process.env.SERVER_ADMIN_USER,
-          password: process.env.SERVER_PASSWORD
-        },
-        type: "default"
-      },
-      server: process.env.SERVER_NAME, 
-      options: {
-        database: process.env.DATABASE_NAME, 
-        encrypt: true,
-        rowCollectionOnDone: true
+      type: "default",
+      options: {  
+        userName: process.env.SERVER_ADMIN_USER,
+        password: process.env.SERVER_PASSWORD,
+      }
     }
-};
+  };
+
+
+// const config = {
+//     userName: process.env.SERVER_ADMIN_USER,
+//     password: process.env.SERVER_PASSWORD,
+//     server: process.env.SERVER_NAME,
+//     options: { encrypt: true, database: process.env.DATABASE_NAME }
+// }
 
 const connection = new Connection(config);
 
