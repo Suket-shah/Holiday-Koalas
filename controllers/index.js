b708b3138352e756fbf4318eb189987f8ad26b90
Suket-shah <54611415+Suket-shah@users.noreply.github.com>
2021-01-08 11:40:09 -0600

    Added Google Analytics

--- controllers/index.js
+++ controllers/index.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 exports.getHome = (req, res, next) => {
     res.render(path.join(__dirname, '../index.ejs'), {
-        data: 'astley.jpg',
+        data: 'koalas1.jpg',
         link: null
     })    
 };
