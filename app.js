62ab4cb6d7c9d2c0c7a710dd0dabf9bdc570e7bb
Suket-shah <54611415+Suket-shah@users.noreply.github.com>
2021-01-12 12:55:02 -0600

    Update port

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const envio = require('dotenv').config();
 
 // express config
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 80;
 
 // middlewear
 app.use(express.static(path.join(__dirname, 'public')));
