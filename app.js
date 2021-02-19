9117fd7427e0020bb2f08fdbf7ca42995b1583e8
Suket-shah <54611415+Suket-shah@users.noreply.github.com>
2021-02-19 12:03:19 -0600

    path .resolve

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // middlewear
-app.use( express.static(path.join(__dirname, 'public')));
+app.use( express.static(path.resolve('public')));
 app.use(express.urlencoded());
 
 glob.sync('./routes/*.js').forEach((file) => {
